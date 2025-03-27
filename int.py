61ab86efff0e9c6b0fca5d075e11208978f4f257
HiChelPlay <sivachevartem@mail.ru>
2025-03-27 17:33:44 +0300

    UPD_1

---
+++ int.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'int.ui'
+##
+## Created by: Qt User Interface Compiler version 6.8.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QLabel, QMainWindow, QMenuBar,
+    QPushButton, QSizePolicy, QStatusBar, QWidget)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.pushButton = QPushButton(self.centralwidget)
+        self.pushButton.setObjectName(u"pushButton")
+        self.pushButton.setGeometry(QRect(310, 480, 171, 41))
+        self.label = QLabel(self.centralwidget)
+        self.label.setObjectName(u"label")
+        self.label.setGeometry(QRect(18, 35, 561, 421))
+        self.label.setPixmap(QPixmap(u"../../Downloads/pngwing.com (2).png"))
+        self.label.setScaledContents(True)
+        self.label_2 = QLabel(self.centralwidget)
+        self.label_2.setObjectName(u"label_2")
+        self.label_2.setGeometry(QRect(100, 290, 71, 81))
+        self.label_2.setPixmap(QPixmap(u"../../Downloads/pngwing.com (3).png"))
+        self.label_2.setScaledContents(True)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QMenuBar(MainWindow)
+        self.menubar.setObjectName(u"menubar")
+        self.menubar.setGeometry(QRect(0, 0, 800, 33))
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QStatusBar(MainWindow)
+        self.statusbar.setObjectName(u"statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
+        self.label.setText("")
+        self.label_2.setText("")
+    # retranslateUi
+
--- test.py
+++ test.py
@@ -1,4 +1,14 @@
+import sys
+from time import sleep
+
+from PySide6.QtWidgets import (QApplication, QMainWindow, QMessageBox)
+from PySide6.QtCore import QRect
+from PySide6.QtGui import QStandardItemModel, QStandardItem
+
+
 import random
+
+from int import Ui_MainWindow
 
 _start = [0, 0, 0, 0]  # человек, коза, капуста, волк
 _goal = [1, 1, 1, 1]  # 0 - левый берег, 1 - правый берег
@@ -8,19 +18,17 @@ states = [current.copy()]
 
 def check(cur, moved_item):
     lose_conditions = [
-        [0, 1, 1, 0],  # человек на левом, коза и капуста на правом
-        [1, 0, 0, 1],  # человек на правом, коза и капуста на левом
-        [0, 1, 0, 1],  # человек на левом, коза и волк на правом
-        [1, 0, 1, 0]  # человек на правом, коза и волк на левом
+        [0,1,1,0], [0,1,1,1],
+        [0,1,0,1], [0,1,1,1],
+        [1,0,0,0], [1,0,1,0],
+        [1,0,0,1], [1,0,0,0]
     ]
 
     if cur in lose_conditions:
-        print(f"You lose! Invalid state: {cur}")
         return False
 
     if cur not in states:
         states.append(cur.copy())
-        print(f"New state: {cur}")
     return True
 
 
@@ -33,7 +41,8 @@ while current != _goal:
         # Перевозим на правый берег
         new_state = current.copy()
         new_state[0] = 1
-        new_state[item_to_move] = 1
+        if random.random() < 0.5:
+            new_state[item_to_move] = 1
     else:
         # Возвращаемся на левый берег (можем взять или не взять предмет)
         new_state = current.copy()
@@ -49,7 +58,6 @@ while current != _goal:
         # При проигрыше начинаем заново
         current = _start.copy()
         states = [current.copy()]
-        print("Restarting game...")
         continue
 
 # После выхода из цикла добавляем конечное состояние
@@ -62,3 +70,32 @@ for i, state in enumerate(states):
 
 print("\nFinal state:", current)
 print("You win!")
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        # Создание UI
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        # Подключение сигналов
+        self.ui.pushButton.clicked.connect(self.button_clicked)
+
+
+    goatPos = 100
+    def button_clicked(self):
+        for i, state in enumerate(states):
+            goatPos = self.ui.label_2.pos().x()
+            if state[3] == 1:
+                self.ui.label_2.setGeometry(QRect(330, 290, 71, 81))
+            else:
+                self.ui.label_2.setGeometry(QRect(100, 290, 71, 81))
+            sleep(0.2)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
